f73ca3780e7bc774d9d0219724ef4b1126fe6370
AdluAghnia <adluaghnia03@gmail.com>
2024-08-28 17:08:46 +0700

    Go To Definision

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,7 @@
-vim.keymap.set("n", "<C-e>", ":Neotree filesystem reveal left toggle<CR>")
-vim.keymap.set("n", "<leader>e", "Neotree focuse<CR>")
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left toggle<CR>")
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>th", ":Telescope colorscheme<CR>")
 vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, {})
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
