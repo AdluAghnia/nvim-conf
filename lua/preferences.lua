1ffdac3d43d18d4866fe8c913cf5e71718e50238
AdluAghnia <adluaghnia03@gmail.com>
2024-09-01 17:25:32 +0700

    Tabstop to 4, Tabstop 6 too much for me

--- lua/preferences.lua
+++ lua/preferences.lua
@@ -1,5 +1,12 @@
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set noexpandtab")
-vim.cmd("set tabstop=6")
+vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+function Transparent()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+Transparent()
